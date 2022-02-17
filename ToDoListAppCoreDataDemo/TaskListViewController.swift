dce319999a965f1c8f2d2456d4e7f355f71ce91e
wwexil <sk.rel@yandex.ru>
2022-02-17 16:53:55 +0300

    1. Prepare app 2. Setup NavigationBar

---
+++ ToDoListAppCoreDataDemo/TaskListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  ToDoListAppCoreDataDemo
+//
+//  Created by Алекс Мерсер on 17.02.2022.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        
+        navBarAppearance.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+
+        
+    }
+}
+
